96e06d43c795bf987f0752098b7ebff1c11287a9
moxian <moxian@users.noreply.github.com>
2025-12-27 10:55:56 -0800

    Bump genemichaels to 0.8.5

--- src/formatting.rs
+++ src/formatting.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use genemichaels::FormatConfig;
 
 pub fn format(file: syn::File) -> anyhow::Result<String> {
     Ok(
-        genemichaels::format_ast(file, &FormatConfig::default(), HashMap::new())
+        genemichaels::format_ast(file, &FormatConfig::default(), BTreeMap::new())
             .map_err(|e| anyhow::format_err!("formatting source file: {:?}", e))?
             .rendered,
     )
